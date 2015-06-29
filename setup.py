8ddc448a23436d1b4f581ffbe5ac9be493839dc6
Konrad Rotkiewicz <konrad.rotkiewicz@gmail.com>
2015-06-29 10:18:18 +0200

    add description

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='gaetasks',
     version='0.0.1',
+    description='Better google appengine deferred lib',
     classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
     ],
     keywords='appengine gae defer deferred',
     author='Konrad Rotkiewicz',

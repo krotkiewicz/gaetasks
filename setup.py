f13ebfeae61e9e65586abc77dc2b91220c0f0258
Konrad Rotkiewicz <konrad.rotkiewicz@gmail.com>
2015-06-29 10:10:06 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gaetasks',
+    version='0.0.1',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='appengine gae defer deferred',
+    author='Konrad Rotkiewicz',
+    author_email='konrad.rotkiewicz@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+)
